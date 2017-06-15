1a5704d5bc6d8c0d287f2e24a4f8f9f09d93023c
jhon0701 <cjhon0701@gmail.com>
2017-06-15 13:41:32 -0400

    boos

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   #root to: 'sign_up#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
-    root :to => 'devise/sessions#new'
+    root :to => 'boos#index'
   end
 end
